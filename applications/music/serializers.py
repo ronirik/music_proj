f481ffb9b5a7d26270a4791d6b440a64f5cb0638
ronirik <roni_rik@mail.ru>
2022-02-18 20:16:14 +0600

    Music project

---
+++ applications/music/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+from applications.music.models import Music, Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ('title', 'year')
+
+
+class MusicSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Music
+        fields = '__all__'
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation.pop('id')
+        # representation['country'] = 'NuBaS'
+        representation['country'] = CategorySerializer(Category.objects.get(music=instance.id)).data
+        return representation
